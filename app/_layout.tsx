03e40d713daa6c02661b88b720b7ca1f86a9741c
lisandro <salvareschilisandro0@gmail>
2025-01-19 23:21:46 -0300

    change config header shown

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(aux)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
